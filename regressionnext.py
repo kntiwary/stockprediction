7d589355857f5ce2833337ff6857912ef8b47011
Kamta Nath Tiwary <Kamta@skillworks.in>
2019-03-21 18:57:15 +0530

    pickled

--- regressionnext.py
+++ regressionnext.py
@@ -1,9 +1,13 @@
+import arrow
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing,svm,model_selection
 from sklearn.linear_model import LinearRegression
-import math
+import math,datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
 
 #preprocessing---->for sclaling data usuallu done on feature, helps us with accuracy and processig speed
 #model_selection/cross_validation ----> creat and test training sample /sample/ good for seperation of data and stats
@@ -29,23 +33,29 @@ df = df[['close', 'HL_PCT', 'PCT_change', 'volume']]
 forcast_col = 'close'
 df.fillna(-99999,inplace=True)
 forcast_out = int(math.ceil(0.01*len(df)))
-print(forcast_out)
+# print(forcast_out)
 #
 df['label']= df[forcast_col].shift(-forcast_out)
-df.dropna(inplace=True)
+
 #
 # print(df.head())
 #<<<<<<<<<<<<----------------------------Train and test------------------>>>>>>>>>>
 
 
 # Drop everything except Lable
-X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
+# print(df.drop('label',axis=1))
+X = np.array(df.drop('label',axis=1))
+# print(X)
+X = preprocessing.scale(X)
+X_lately =X[-forcast_out:]
+X=X[:-forcast_out] # stuff that we are going to predict again so we have x , we need to figure out m and b
 
+
+y = np.array(df['label'])
 
 #scale X before feeding it to through classifier, only for real time data , helps in testing and traing and also processing time we can skip these for high frequcy trading
-X = preprocessing.scale(X)
-y = np.array(df['label'])
+
+df.dropna(inplace=True)
 # print(len(X),len(y))
 
 # This is going to take our features and label and shuffle them up keeping x and y connected and outputs X_train,y _train we use to fit classifier
@@ -71,9 +81,35 @@ clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train,y_train)
 accuracy = clf.score(X_test,y_test)
 
-print(accuracy)
+# print(accuracy)
 
 
 #--------------->>   prediction in future--------------
+#predict based on x
+forcast_set = clf.predict(X_lately) # we can either pass a single value of an array
 
+print(forcast_set,accuracy,forcast_out)
+df['Forcast '] =np.nan
+
+last_date = df.iloc[-1].name
+# last_unix = last_date.timestamp()
+last_unix = arrow.get(last_date).timestamp
+one_day = 86400
+next_unix = last_unix + one_day # its like next day
+
+#
+# for i in forcast_set:
+#     next_date = datetime.datetime.fromtimestamp(next_unix)
+
+#     next_unix += next_date
+#     df.loc[next_date]=[np.nan for _ in range(len(df.columns)-1)] +[i]
+#
+#
+# df['close'].plot()
+# df['Forcast'].plot()
+# plt.legend(loc=4)
+# plt.xlabel('Date')
+# plt.ylabel('Price')
+# plt.show()
+#
 
